f9f0b072526c59ca33a14b45a28a4a060f0e7b38
city535377 <112466916+city535377@users.noreply.github.com>
2022-10-02 10:38:28 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,21 @@ let classifier;
 // Model URL
 let imageModelURL = 'https://teachablemachine.withgoogle.com/models/hk59cAHml/';
 
-  var constraints = {
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = "";
+
+// Load the model first
+function preload() {
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+}
+
+function setup() {
+  createCanvas(320, 320);
+  // Create the video
+    var constraints = {
 
     audio: false,
 
@@ -36,22 +50,9 @@ let imageModelURL = 'https://teachablemachine.withgoogle.com/models/hk59cAHml/';
 
   };
 
-
-// Video
-let video;
-let flippedVideo;
-// To store the classification
-let label = "";
-
-// Load the model first
-function preload() {
-  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
-}
-
-function setup() {
-  createCanvas(320, 260);
-  // Create the video
-  video = createCapture(VIDEO);
+  
+  
+  video = createCapture(constraints);
   video.size(320, 240);
   video.hide();
 
